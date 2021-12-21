16898e8eff47ee08fc302d32aa4bd3e64de27551
Ysaakue <isaacgondim82@gmail.com>
2021-12-21 17:56:24 -0300

    test data acquisition

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@ int n;
 int it;
 
 int main() {
-    mn::CppLinuxSerial::SerialPort serialPort("/dev/ttyUSB0", mn::CppLinuxSerial::BaudRate::B_115200);
+//    mn::CppLinuxSerial::SerialPort serialPort("/dev/ttyUSB0", mn::CppLinuxSerial::BaudRate::B_115200);
+    mn::CppLinuxSerial::SerialPort serialPort("/dev/ttymxc4", mn::CppLinuxSerial::BaudRate::B_115200);
     serialPort.SetTimeout(-1);
     serialPort.Open();
     QByteArray local_buffer;
+    int count = 0;
+    QTime time = QTime::currentTime();
+    union {
+        int16_t number;
+        uint8_t array[2];
+    } converter;
 
     while(1){
         // Lendo byte a byte
@@ -25,18 +32,20 @@ int main() {
             // Se o último byte recebido for um final de pacote
             if(n.mid(0,1) == "\x55")
             {
-                if(local_buffer.mid(0,1) == "\xaa"){
-                    double pressure;
-                    union {
-                        int16_t number;
-                        uint8_t array[2];
-                    } converter;
-
+                if(local_buffer.mid(0,1) == "\xaa")
+                {
                     converter.array[0] = local_buffer[2];
                     converter.array[1] = local_buffer[1];
-                    pressure = converter.number/10.0;
 
-                    qDebug() << "PRESSÃO: " << pressure; // Exibe valor recebido
+                    count++;
+//                    if(count == 500){
+//                        qDebug() << converter.number/10.0;; // Exibe valor recebido
+//                        count = 0;
+//                    }
+                    int t = time.msecsTo(QTime::currentTime());
+                    if(t > 1)
+                        qDebug() << "t: " << t;
+                    time = QTime::currentTime();
                 }
                 local_buffer.clear();
             }
